1d2f41a688ac3f22e1a4d1fb206a6c1a0f7a8a74
Suman Rajmohan <sumanrajmohan@Sumans-MacBook-Pro.local>
2015-10-15 15:03:37 -0400

    Suman: First commit returning a static page for / and /hello.html

---
+++ index.js
@@ -0,0 +1,24 @@
+//1
+var http = require('http'),
+	 express = require('express'),
+	 path = require('path');;
+
+var app = express();
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (req, res) {
+  res.send('<html><body><h1>Hello World</h1></body></html>');
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});	 
+ 
+//2 
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   res.end('<html><body><h1>Hello World</h1></body></html>');
+// }).listen(3000);
+ 
+// console.log('Server running on port 3000.');
